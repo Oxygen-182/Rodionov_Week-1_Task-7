863c08ff874670834f40f59edde6c474ae2e95ed
Oxygen-182 <119591922+Oxygen-182@users.noreply.github.com>
2022-12-16 09:12:49 +0300

    Add files via upload

---
+++ ConsoleApp_7_1/ConsoleApp_7_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку, содержащую 2 запятые: ");
+            string text = Console.ReadLine();
+
+            int count = 0;
+            foreach (char c in text)
+                if (c == ',') count++;
+
+            if (count != 2)
+            {
+                Console.WriteLine("Нужно ввести две запятые!");
+                return;
+            }
+
+            int pos1 = text.IndexOf(',');
+            int pos2 = -1;
+            if (pos1 >= 0)
+                pos2 = text.IndexOf(',', pos1 + 1);
+            if (pos1 >= 0 && pos2 >= 0)
+                text = text.Remove(pos1 + 1, pos2 - pos1 - 1);
+            Console.WriteLine($"Измененная строка: {text}");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ ConsoleApp_7_1_form/ConsoleApp_7_1_form/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp_7_1_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+
+            string s = textBox1.Text;
+
+            int count = 0;
+            foreach (char c in s)
+                if (c == ',') count++;
+
+            if (count != 2)
+            {
+                textBox2.Text += "Нужно ввести две запятые!";
+                return;
+            }
+
+            int pos1 = s.IndexOf(',');
+            int pos2 = -1;
+            if (pos1 >= 0)
+                pos2 = s.IndexOf(',', pos1 + 1);
+            if (pos1 >= 0 && pos2 >= 0)
+                s = s.Remove(pos1 + 1, pos2 - pos1 - 1);
+
+            textBox2.Text += s;
+        }
+    }
+}
